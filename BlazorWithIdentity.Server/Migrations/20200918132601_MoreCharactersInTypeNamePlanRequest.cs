63e991d15b8fc26cbd524b2232dda99d35f76f14
Kamil Gąsiorek <kgasiorek@bomadek.com.pl>
2020-09-18 15:56:36 +0200

    AddPlanWorksGreat

---
+++ BlazorWithIdentity.Server/Migrations/20200918132601_MoreCharactersInTypeNamePlanRequest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorWithIdentity.Server.Migrations
+{
+    public partial class MoreCharactersInTypeNamePlanRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "WeightingPlans",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "WeightingPlans",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+        }
+    }
+}
