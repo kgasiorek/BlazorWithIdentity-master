b7c31933c514889a120da853b0fc01647d029e3c
Kamil Gąsiorek <kgasiorek@bomadek.com.pl>
2020-09-18 14:36:07 +0200

    SecondCommit

---
+++ BlazorWithIdentity.Shared/Models/OperationType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BlazorWithIdentity.Shared.Models
+{
+    class OperationType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string TypeShort { get; set; }
+    }
+}
--- BlazorWithIdentity.Shared/Models/OperationTypeRequest.cs
+++ BlazorWithIdentity.Shared/Models/OperationTypeRequest.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorWithIdentity.Shared.Models
 {
 
     public class OperationTypeRequest
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Typ jest wymagany")]
+        [StringLength(10)]
         public string Type { get; set; }
+
+        [Required(ErrorMessage = "Skrót typu jest wymagany")]
+        [StringLength(2)]
+        public string TypeShort { get; set; }
     }
 }
